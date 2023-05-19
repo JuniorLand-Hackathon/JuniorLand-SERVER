387c1048c8eb46e18b6befeb751fc854cb0437dc
nuyh <hjaehyun25@gmail.com>
2023-05-19 20:58:53 +0900

    feat: 전역 예외 핸들러 추가

---
+++ src/main/java/com/hackerton/junior/controller/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.hackerton.junior.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public final class GlobalExceptionHandler {
+    private final Logger logger = LoggerFactory.getLogger("!!서버 에러!!");
+
+    @ExceptionHandler
+    public ResponseEntity<String> handle(final Exception e) {
+        logger.error("서버 에러: ", e);
+
+        return ResponseEntity.internalServerError().body("서버 에러 발생 띠용");
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handle(final IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
